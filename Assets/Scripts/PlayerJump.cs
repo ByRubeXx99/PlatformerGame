9ca4fc429558a2ee630431b6677e17b488cf18ef
sashasoler <sashasoler24@gmail.com>
2026-01-27 15:23:25 +0100

    PlayerJump implemented

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float JumpHeight;
+    public float DistanceToMaxHeight;
+    public float SpeedHorizontal;
+    public float PressTimeToMaxJump;
+    public float WallSlideSpeed = 1;
+    public ContactFilter2D filter;
+
+    private Rigidbody2D rb;
+    private CollisionDetection collisionDetection;
+    private float lastVelocityY;
+    private float jumpStartedTime;
+
+    bool IsWallSliding => collisionDetection.IsTouchingFront;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collisionDetection = GetComponent<CollisionDetection>();
+    }
+
+   void FixedUpdate()
+    {
+        if (IsPeakReached()) TweakGravity();
+
+        if (IsWallSliding) SetWallSlide();
+    }
+
+    public void OnJumpStarted()
+    {
+        SetGravity(); 
+        var velocity = new Vector2(rb.linearVelocity.x, GetJumpForce());
+        rb.linearVelocity = velocity;
+        jumpStartedTime = Time.time;
+    }
+
+    public void OnJumpFinished()
+    {
+        float fractionOfTimePressed = 1 / Mathf.Clamp01((Time.time - jumpStartedTime) / PressTimeToMaxJump);
+        rb.gravityScale *= fractionOfTimePressed;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        float h = -GetDistanceToGround() + JumpHeight;
+        Vector3 start = transform.position + new Vector3(-1, h, 0);
+        Vector3 end = transform.position + new Vector3(1, h, 0);
+        Gizmos.DrawLine(start, end);
+        Gizmos.color = Color.white;
+    }
+    
+    private bool IsPeakReached()
+    {
+        bool reached = ((lastVelocityY * rb.linearVelocity.y) < 0);
+        lastVelocityY = rb.linearVelocity.y;
+
+        return reached;
+    }
+
+    private void SetWallSlide()
+    {
+        //rigidbody.gravityScale = 0.8f;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 
+            Mathf.Max(rb.linearVelocity.y, -WallSlideSpeed));
+    }
+
+    private void SetGravity()
+    {
+        var grav = 4 * JumpHeight * (SpeedHorizontal * SpeedHorizontal) / (DistanceToMaxHeight * DistanceToMaxHeight);
+        rb.gravityScale = grav / 9.81f;
+    }
+
+    private void TweakGravity()
+    {
+        rb.gravityScale *= 1.2f;
+    }
+
+    private float GetJumpForce()
+    {
+        return 2 * JumpHeight * SpeedHorizontal / DistanceToMaxHeight;
+    }
+
+    private float GetDistanceToGround()
+    {
+        RaycastHit2D[] hit = new RaycastHit2D[3];
+
+        Physics2D.Raycast(transform.position, Vector2.down, filter, hit, 10);
+
+        return hit[0].distance;
+    }
+}
